d6561bc463f50e1733c2a24ffb7d95c36c299c6e
eunannana <annisa.divayu.ft20@mail.umy.ac.id>
2023-05-15 00:58:06 +0700

    add id for the model

--- lib/model/kategori_barang_model.dart
+++ lib/model/kategori_barang_model.dart
@@ -2,15 +2,19 @@ import 'dart:convert';
 
 class KategoriBarangModel {
   final String nama;
+  var id;
   KategoriBarangModel({
     required this.nama,
+    this.id,
   });
 
   KategoriBarangModel copyWith({
     String? nama,
+    int? id,
   }) {
     return KategoriBarangModel(
       nama: nama ?? this.nama,
+      id: id ?? this.id,
     );
   }
 
@@ -23,6 +27,7 @@ class KategoriBarangModel {
   factory KategoriBarangModel.fromMap(Map<String, dynamic> map) {
     return KategoriBarangModel(
       nama: map['nama'] as String,
+      id: map['id_kategori_barang']?.toInt() ?? 0,
     );
   }
 
@@ -31,14 +36,15 @@ class KategoriBarangModel {
   factory KategoriBarangModel.fromJson(String source) => KategoriBarangModel.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() => 'KategoriBarangModel(nama: $nama)';
+  String toString() => 'KategoriBarangModel(nama: $nama, id: $id)';
 
   @override
   bool operator ==(covariant KategoriBarangModel other) {
     if (identical(this, other)) return true;
   
     return 
-      other.nama == nama;
+      other.nama == nama &&
+      other.id == id;
   }
 
   @override
