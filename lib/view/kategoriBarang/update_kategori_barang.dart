14266d3fbe894448e4946819debfc7c338223c15
eunannana <annisa.divayu.ft20@mail.umy.ac.id>
2023-05-15 01:18:32 +0700

    modify update_kategori_barang.dart

--- lib/view/kategoriBarang/update_kategori_barang.dart
+++ lib/view/kategoriBarang/update_kategori_barang.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:exerciseapi_136/view/kategoriBarang/kategori_barang.dart';
 
 class updateKategoriBarang extends StatefulWidget {
   const updateKategoriBarang({super.key});
@@ -11,6 +11,47 @@ class updateKategoriBarang extends StatefulWidget {
 class _updateKategoriBarangState extends State<updateKategoriBarang> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var formkey = GlobalKey<FormState>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Update Category'),
+      ),
+      body: Form(
+        key: formkey,
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Category Name',
+                labelText: 'Category Name',
+              ),
+              onChanged: (value) {},
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Category Name is required';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                if (formkey.currentState!.validate()) {
+                  formkey.currentState!.save();
+                  Navigator.pop(context,
+                      MaterialPageRoute(
+                          builder: (context) => const KategoriBarang()));
+                  var snackBar =
+                      const SnackBar(content: Text('Successfully update data!'));
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
