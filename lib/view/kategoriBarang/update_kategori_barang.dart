d3a12da8264d03ab7176f6c3f580dc93c12e2490
eunannana <annisa.divayu.ft20@mail.umy.ac.id>
2023-05-15 01:11:11 +0700

    create update page

---
+++ lib/view/kategoriBarang/update_kategori_barang.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class updateKategoriBarang extends StatefulWidget {
+  const updateKategoriBarang({super.key});
+
+  @override
+  State<updateKategoriBarang> createState() => _updateKategoriBarangState();
+}
+
+class _updateKategoriBarangState extends State<updateKategoriBarang> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
